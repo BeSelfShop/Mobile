2b8f4066f5a569558caf8980ddf59aa8bfc6c41e
sebcwi <61754816+sebcwi@users.noreply.github.com>
2021-04-19 15:35:16 +0200

    Revert "login <3"
    This reverts commit a64caa78c64936a630abb5494e43b3a5e672199b.

--- Prison/app/src/main/java/storage/SharedPrefManager.kt
+++ Prison/app/src/main/java/storage/SharedPrefManager.kt
@@ -1,48 +1,49 @@
 package storage
 
 import android.content.Context
-import models.LoginResponse
+import models.User
 
 
 class SharedPrefManager private constructor(private val mCtx: Context) {
 
-    val isLoggedIn: Boolean?
+    val isLoggedIn: String?
         get() {
-            val sharedPreferences = mCtx.getSharedPreferences("SHARED_PREF_NAME", Context.MODE_PRIVATE)
-            return sharedPreferences.getBoolean("isLogin",false)
+            val sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
+            return sharedPreferences.getString("token", "")
         }
 
-    val user: LoginResponse
+    val user: User
         get() {
-            val sharedPreferences = mCtx.getSharedPreferences("SHARED_PREF_NAME", Context.MODE_PRIVATE)
-            return LoginResponse(
-                    sharedPreferences.getString("token",null),
-                    sharedPreferences.getString("expiration",null),
-                sharedPreferences.getString("roles",null)
+            val rule: List<String>
+            val sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
+            return User(
+                sharedPreferences.getString("token"," "),
+                sharedPreferences.getString("exp"," ")
             )
         }
 
 
-    fun saveUser(user: LoginResponse?) {
+    fun saveUser(user: User) {
 
-        val sharedPreferences = mCtx.getSharedPreferences("SHARED_PREF_NAME", Context.MODE_PRIVATE)
+        val sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
         val editor = sharedPreferences.edit()
 
-        editor.putString("tokken", user?.token)
-        editor.putBoolean("isLogin",true)
+        editor.putString("tokken", user.token)
+        editor.putString("exp", user.expiration)
 
         editor.apply()
 
     }
 
     fun clear() {
-        val sharedPreferences = mCtx.getSharedPreferences("SHARED_PREF_NAME", Context.MODE_PRIVATE)
+        val sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
         val editor = sharedPreferences.edit()
         editor.clear()
         editor.apply()
     }
 
     companion object {
+        private val SHARED_PREF_NAME = "my_shared_preff"
         private var mInstance: SharedPrefManager? = null
         @Synchronized
         fun getInstance(mCtx: Context): SharedPrefManager {
